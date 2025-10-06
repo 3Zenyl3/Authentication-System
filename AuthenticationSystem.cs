428d8bb681b0578103c1f728349635cbd6b25536
3Zenyl3 <zenyl2007@gmail.com>
2025-10-06 13:06:59 +0500

    Create AuthenticationSystem.cs

---
+++ AuthenticationSystem.cs
@@ -0,0 +1,53 @@
+using System.Collections.ObjectModel;
+public class Authenticator
+{
+    private class EyeColor
+    {
+        private const string Blue = "blue";
+        private const string Green = "green";
+        private const string Brown = "brown";
+        private const string Hazel = "hazel";
+        private const string Grey = "grey";
+    }
+
+    public Authenticator(Identity admin)
+    {
+        this.admin = admin;
+    }
+
+    private Identity admin;
+
+    private readonly IDictionary<string, Identity> developers
+        = new Dictionary<string, Identity>
+        {
+            ["Bertrand"] = new Identity
+            {
+                Email = "bert@ex.ism",
+                EyeColor = "blue"
+            },
+
+            ["Anders"] = new Identity
+            {
+                Email = "anders@ex.ism",
+                EyeColor = "brown"
+            }
+        };
+
+    public Identity Admin
+    {
+        get { return admin; }
+        set { admin = value; }
+    }
+
+    public ReadOnlyDictionary<string, Identity> GetDevelopers()
+    {
+        return new ReadOnlyDictionary<string, Identity>(developers);
+    }
+}
+
+public struct Identity
+{
+    public string Email { get; set; }
+
+    public string EyeColor { get; set; }
+}
